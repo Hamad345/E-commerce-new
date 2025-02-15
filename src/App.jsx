fff44bf56659c18d1cb5e2f275bf23ba6a793144
HamadIslamian <hamad.yousafzai34@gmail.com>
2025-02-15 15:11:09 +0500

    next

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import { Routes,Route } from 'react-router'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Footer from './components/Footer'
+import SearchBar from './components/SearchBar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -23,6 +24,7 @@ function App() {
     <>
 <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] '>
   <Navbar/>
+  <SearchBar/> 
   <Routes>
     <Route path='/'  element={<Home/>} />
      <Route path='/collection'  element={<Collection/>} />
